5460cbcf367e79e26fdc3893735d45aff51c344a
quockhanh22 <anhkhanh11761@gmail.com>
2020-09-28 12:04:52 +0700

    first commit

---
+++ Linq_ArrayList/Staff.cs
@@ -0,0 +1,9 @@
+﻿namespace SortedListKT
+{
+    internal class Staff
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
